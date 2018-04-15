8900aa14d82425bf88061cb7e24a3a1c38f2c31c
Axolotle <nchesnais@mailoo.org>
2018-04-15 22:57:06 +0200

    Add scroll to elem on click

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+function showContent(a) {
+    var elem = content[Array.prototype.indexOf.call(targ, a.target)];
+
+    if (last === undefined) {
+        document.getElementsByTagName("MAIN")[0].classList.add("after");
+        document.getElementsByTagName("NAV")[0].classList.add("after");
+    } else {
+        last.style.display = "none";
+    }
+
+    elem.style.display = "block";
+    if (elem.scrollIntoView === undefined) {
+        window.scrollTo(0, document.getElementsByTagName("NAV")[0].offsetHeight);
+    } else {
+        elem.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+    last = elem;
+}
+
 var targ = document.getElementsByTagName("LI");
 var content = document.getElementsByClassName("content");
 var last;
 
-function showContent(a) {
-    var i = Array.prototype.indexOf.call(targ, a.target);
-
-    if (last != undefined) {
-        last.style.display = "none";
-        last = content[i];
-        content[i].style.display = "block";
-    } else {
-        document.getElementsByTagName("MAIN")[0].classList.add("after");
-        document.getElementsByTagName("NAV")[0].classList.add("after");
-        content[i].style.display = "block";
-        last = content[i];
-    }
-}
-
 for (var i = 0; i < targ.length; i++) {
     targ[i].addEventListener("click", showContent);
+}
+document.getElementsByClassName("contact")[0].onclick = function () {
+    showContent({target: targ[targ.length - 1]})
 }
